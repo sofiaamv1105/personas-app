bc81c6a7c10cf7e9329102a61e30928523c86e06
Sofia <sofiaamv2003@gmail.com>
2025-03-26 12:49:45 -0500

    Commune Delete

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -27,8 +27,8 @@ class DepartamentoController extends Controller
     public function create()
     { 
         $paises = DB::table('tb_pais')
-            ->orderBy('pais_nomb')
-            ->get();
+        ->orderBy('pais_nomb')
+        ->get();
         return view('departamento.new', ['paises' => $paises]); 
     }
 
@@ -37,17 +37,17 @@ class DepartamentoController extends Controller
      */
     public function store(Request $request)
     {
-        $departamento = new Departamento();
-        $departamento->depa_nomb = $request->name;
-        $departamento->pais_codi = $request->code;
-        $departamento->save();
+       $departamento = new Departamento();
     
-        $departamentos = DB::table('tb_departamento')
-            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
-            ->select('tb_departamento.*', "tb_pais.pais_nomb")
-            ->get();
-    
-        return view('departamento.index', ['departamentos' => $departamentos]);
+    $departamento->depa_nomb = $request->name;
+    $departamento->pais_codi = $request->code;
+    $departamento->save();
+
+    $departamentos = DB::table('tb_departamento')
+        ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+        ->select('tb_departamento.*', "tb_pais.pais_nomb")
+        ->get();
+    return view('departamento.index', ['departamentos' => $departamentos]);
     }
 
     /**
@@ -80,16 +80,13 @@ class DepartamentoController extends Controller
     public function destroy(string $id)
     {
         $departamento = Departamento::find($id);
-
-    if ($departamento) {
         $departamento->delete();
-    }
-
-    $departamentos = DB::table('tb_departamento')
-        ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
-        ->select('tb_departamento.*', "tb_pais.pais_nomb")
-        ->get();
-
-    return view('departamento.index', ['departamentos' => $departamentos]);
+    
+        $departamentos = DB::table('tb_departamento')
+            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+            ->select('tb_departamento.*', "tb_pais.pais_nomb")
+            ->get();
+    
+        return view('departamento.index', ['departamentos' => $departamentos]);
     }
 }
--- resources/views/departamento/new.blade.php
+++ resources/views/departamento/new.blade.php
@@ -1,42 +1,42 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1An3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Add Department</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Add Departament</title>
 
   </head>
   <body>
     <div class="container">
       <h1>Add Department</h1>
       <form method="POST" action="{{ route('departamentos.store') }}">
-          @csrf
-          <div class="mb-3">
-              <label for="id" class="form-label">Code</label>
-              <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
-              <div id="idHelp" class="form-text">Department code</div>
-          </div>
-          <div class="mb-3">
-              <label for="name" class="form-label">Department</label>
-              <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
-                  name="name" placeholder="Department name.">
-          </div>
-          <label for="country">Country:</label>
-          <select class="form-select" id="country" name="code" required>
-              <option selected disabled value="">Choose one...</option>
-              @foreach ($paises as $pais)
-                  <option value="{{ $pais->pais_codi }}">{{ $pais->pais_nomb }}</option>
-              @endforeach
-          </select>
-          <div class="mt-3">
-              <button type="submit" class="btn btn-primary">Save</button>
-              <a href="{{ route('departamentos.index') }}" class="btn btn-warning">Cancel</a>
-          </div>
-      </form>
+            @csrf
+            <div class="mb-3">
+                <label for="id" class="form-label">Code</label>
+                <input type="text" class="form-control" id="id" aria-describedby="idHelp" name="id" disabled="disabled">
+                <div id="idHelp" class="form-text">Departament code</div>
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Departament</label>
+                <input type="text" required class="form-control" id="name" aria-describedby="nameHelp"
+                    name="name" placeholder="Comuna name.">
+            </div>
+            <label for="country">Country:</label>
+            <select class="form-select" id="country" name="code" required>
+                <option selected disabled value="">Choose one...</option>
+                @foreach ($paises as $pais)
+                    <option value="{{ $pais->pais_codi }}">{{ $pais->pais_nomb }}</option>
+                @endforeach
+            </select>
+            <div class="mt-3">
+                <button type="submit" class="btn btn-primary">Save</button>
+                <a href="{{ route('departamentos.index') }}" class="btn btn-warning">Cancel</a>
+            </div>
+        </form>
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
