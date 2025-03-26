d057752de02c9a73f6158db6121411f1b9e64ef2
Sofia <sofiaamv2003@gmail.com>
2025-03-26 13:12:49 -0500

    Department Edit

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -63,7 +63,12 @@ class DepartamentoController extends Controller
      */
     public function edit(string $id)
     {
-        
+        $departamento = Departamento::find($id);
+        $paises = DB::table('tb_pais')
+        ->orderBy('pais_nomb')
+        ->get();
+    
+        return view('departamento.edit', ['departamento' => $comuna, 'paises' => $paises]);
     }
 
     /**
@@ -71,7 +76,18 @@ class DepartamentoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-       
+        $departamento = Departamento::find($id);
+
+        $departamento->depa_nomb = $request->name;
+        $departamento->pais_codi = $request->code;
+        $departamento->save();
+
+        $departamentos = DB::table('departamento')
+            ->join('tb_pais', 'tb_departamento.pais_codi', '=', 'tb_pais.pais_codi')
+            ->select('tb_departamento.*', "tb_pais.pais_nomb")
+            ->get();
+    
+        return view('departamento.index', ['departamentos' => $departamentos]);
     }
 
     /**
